68ed6f024ed63fd0c7769fb481733a570ee2bf1c
Saunak <saunak.nxs@gmail.com>
2023-05-19 11:09:55 +0530

    customise items

--- src/Pages/CustomiseItem.js
+++ src/Pages/CustomiseItem.js
@@ -16,13 +16,27 @@ import { useNavigate } from "react-router-dom";
 const CustomiseItem = () => {
   //Hooks or states
   const [selectedTemplate, setSelectedTemplate] = useState("");
+  const [textareaValue, setTextareaValue] = useState("");
 
   //function calling
   const navigate = useNavigate();
+  const handleTextareaChange = (event) => {
+    setTextareaValue(event.target.value);
+  };
 
   const handleselectedTemplate = (event) => {
     setSelectedTemplate(event.target.value);
   };
+  const currencies = [
+    {
+      value: "No Tax",
+      label: "No Tax",
+    },
+    {
+      value: "Tax able",
+      label: "Tax able",
+    },
+  ];
   // return statement
   return (
     <div className="bg-white h-auto w-[70%] mx-auto border">
@@ -41,7 +55,7 @@ const CustomiseItem = () => {
       </div>
       <p className="text-center text-3xl mt-3 font-semibold">
         {" "}
-        Customise Items
+        Customise items
       </p>
       <form>
         <p className=" mt-3 pl-8 text-lg font-bold  mb-2">Preview</p>
@@ -63,8 +77,57 @@ const CustomiseItem = () => {
           </FormControl>
         </Box>
         <div className="h-56 w-[95%] mx-auto mt-3 border-2 border-gray-300 rounded-xl">
-          <div className=""></div>
-          <div className=""></div>
+          <div className="p-3 ">
+            <div className=" max-w-1/4 flex items-center w-full">
+              <select
+                id="dropdown-select"
+                className="w-60 py-4 px-3 text-base border border-gray-300 rounded-md box-border"
+              >
+                <option value="">None</option>
+                <option value="option1">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+              <input
+                id="outlined-search"
+                type="search"
+                className=" w-1/4 ml-6 border border-gray-400 rounded-md py-4 px-3 placeholder-black"
+                placeholder="Quantity"
+              />
+              <input
+                id="outlined-search"
+                type="search"
+                className=" w-1/4 ml-6 border border-gray-400 rounded-md py-4 px-3 placeholder-black"
+                placeholder="Price"
+              />
+              <form className="ml-6">
+                <div className="w-full">
+                  <select
+                    id="outlined-select-currency"
+                    className="w-full py-4 px-3 border border-gray-400 rounded-md"
+                  >
+                    {currencies.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="">
+            <textarea
+              className="peer block min-h-[auto] w-[97%] mx-auto border border-gray-500 rounded mt-5 text-black px-3 py-[0.32rem]  "
+              id="exampleFormControlTextarea1"
+              rows="4"
+              placeholder="Description(optional)"
+              value={textareaValue}
+              onChange={handleTextareaChange}
+            >
+              {" "}
+            </textarea>
+          </div>
         </div>
         <div className="w-[97%] p-5  mx-auto mt-5">
           <p className="text-lg font-semibold text-gray-700">
